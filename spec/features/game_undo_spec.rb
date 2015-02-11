85e780d949b7a06eeb6065f2a137638dd9867ba9
Erik Olsen <erik.s.olsen@gmail.com>
2015-02-11 17:46:56 -0600

    Refactor game_undo_spec

--- spec/features/game_undo_spec.rb
+++ spec/features/game_undo_spec.rb
@@ -1,49 +1,24 @@
 require 'rails_helper'
 
-describe 'undoing a game' do
+RSpec.describe 'undoing a game' do
   let!(:winner) { Player.create! name: 'winner', rating: 1000 }
   let!(:loser) { Player.create! name: 'loser', rating: 1000 }
-  it 'lets you undo a game if it was incorrectly entered' do
-    visit new_game_path
-    select winner.name, from: :game_winner_id
-    select loser.name, from: :game_loser_id
+  let!(:first_game) { Game.create! winner_id: winner.id,
+                                   loser_id: loser.id,
+                                   winner_rating: winner.rating,
+                                   loser_rating: loser.rating}
+  let!(:last_game) { Game.create! winner_id: winner.id,
+                                   loser_id: loser.id,
+                                   winner_rating: winner.rating,
+                                   loser_rating: loser.rating}
+  context 'will not happen if it is the first game' do
+    it 'will not undo a game unless it is the last game' do
+      expect(GameDestroyer.new(first_game).undo_game!).to be false
+    end
 
-    click_button 'Update Ratings'
-    first_game = Game.last
-
-    winner.reload
-    loser.reload
-
-    expect(page).to have_content('Game created')
-    expect(find('h3')).to have_content('Game Summary')
-
-    visit new_game_path
-    select winner.name, from: :game_winner_id
-    select loser.name, from: :game_loser_id
-    click_button 'Update Ratings'
-    bad_game = Game.last
-
-    winner.reload
-    loser.reload
-    expect(winner.rating).to eq 1047
-    expect(loser.rating).to eq 953
-    
-    click_link 'Undo'
-    expect(page.current_path).to eq "/games/new"
-
-    winner.reload
-    loser.reload
-    expect(winner.rating).to be 1025
-    expect(loser.rating).to be 975
+    it 'will happen if game  is the last game' do
+      expect(GameDestroyer.new(last_game).undo_game!).to be true
+    end
   end
 
-  it 'will not undo a game unless it is the last game' do
-    creator = GameCreator.new(winner.id, loser.id)
-    creator.save
-    first_game = creator.game
-    bad_creator = GameCreator.new(winner.id, loser.id)
-    bad_creator.save
-    bad_game = bad_creator.game
-    expect(GameDestroyer.new(first_game).undo_game!).to be false
-  end
 end
