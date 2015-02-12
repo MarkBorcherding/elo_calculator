02dc72fe68462adc9ef031a9e7605d046546ec5a
Erik Olsen <erik.s.olsen@gmail.com>
2015-02-11 20:39:32 -0600

    Refactor game undo spec

--- spec/features/game_undo_spec.rb
+++ spec/features/game_undo_spec.rb
@@ -13,11 +13,16 @@ RSpec.describe 'undoing a game' do
                                    loser_rating: loser.rating}
   context 'will not happen if it is the first game' do
     it 'will not undo a game unless it is the last game' do
+      visit game_path(first_game.id)
+      expect(page).to_not have_content('Undo Game')
       expect(GameDestroyer.new(first_game).undo_game!).to be false
     end
 
     it 'will happen if game  is the last game' do
-      expect(GameDestroyer.new(last_game).undo_game!).to be true
+      visit game_path(last_game.id)
+      expect(page).to have_content('Undo Game')
+      click_link 'Undo Game'
+      expect(Game.where(id: last_game.id)).to be_empty  
     end
   end
 
