6dd42438262f691348bccc236fae48fd627e66a5
Daniel Errante <danoph@gmail.com>
2015-03-08 13:58:17 -0500

    players join clubs in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,31 @@
 Player.delete_all
 Game.delete_all
 Club.delete_all
+Membership.delete_all
 
 5.times do
   Club.create! name: Faker::Company.name
 end
+
+club_ids = Club.pluck(:id)
 
 20.times do
   Player.create! name: Faker::Name.name, rating: 1000
 end
 
 player_ids = Player.pluck(:id)
+
+player_ids.each do |player_id|
+  clubs_to_join = rand(0..5)
+
+  clubs_joined = []
+
+  clubs_to_join.times do
+    club_id = (club_ids - clubs_joined).sample
+    Membership.create! club_id: club_id, player_id: player_id
+    clubs_joined << club_id
+  end
+end
 
 500.times do
   GameCreator.new(player_ids.sample, player_ids.sample).save
