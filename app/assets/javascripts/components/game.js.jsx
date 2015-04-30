5d1337d4ccda05ad9d30c18f05518de442451e44
Mark Borcherding <markborcherding@gmail.com>
2015-04-29 22:07:31 -0500

    replace games index

---
+++ app/assets/javascripts/components/game.js.jsx
@@ -0,0 +1,12 @@
+var Game = React.createClass({
+
+  render: function() {
+    return <tr align="center">
+            <td className="winner">
+              {this.props.winner}
+            </td>
+            <td className="loser">fo</td>
+            <td className="date">fo</td>
+          </tr>;
+  }
+});
---
+++ app/assets/javascripts/components/games.js.jsx
@@ -0,0 +1,41 @@
+var Games = React.createClass({
+
+  games: function(){
+    return this.props.games.map(function(game){
+      return <tr align="center">
+              <td className="winner">
+                {game.winner_id}
+                ({game.winner_rating})
+              </td>
+              <td className="loser">
+                {game.loser_id}
+                ({game.loser_rating})
+              </td>
+              <td className="date">
+                {game.created_at}
+              </td>
+            </tr>;
+    });
+  },
+
+  render: function() {
+    return <table width="100%">
+            <thead>
+              <tr>
+                <th>
+                  Humble Victor
+                </th>
+                <th>
+                  Graceful Defeated
+                </th>
+                <th>
+                  Date
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+            { this.games() }
+            </tbody>
+          </table>;
+  }
+});
